b2e81e864e8ef4f811bd9f999182ab21aaabf591
vagrant <vagrant@homestead>
2018-06-05 06:40:15 +0000

    第三方登錄可暂不綁定邮箱

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,7 +2,6 @@
 
 namespace App\Http;
 
-use App\Http\Middleware\ThirdLogin;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -36,7 +35,6 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            ThirdLogin::class,
         ],
 
         'api' => [
